b86f40ad8e499946f10b3540342fc753dce5a1d9
Providence Salumu <Providence.Salumu@smunix.com>
2022-10-10 22:07:35 -0400

    add mkCabal function

--- flake.nix
+++ flake.nix
@@ -1,34 +1,46 @@
 {
   description = "nix utils";
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  inputs.nix-filter.url = "github:numtide/nix-filter";
+
+  outputs = { self, nixpkgs, nix-filter, ... }@inputs:
     with nixpkgs.lib;
     let
       composeThen = f: g: x: f (g (x));
       composeMany = foldr composeThen (x: x);
 
       haskellSharedLibExe = pkgs:
-        with pkgs.haskell.lib;
-        rec {
+        with pkgs.haskell.lib; rec {
           __functor = self: sharedLibExe;
-          sharedLibExe =
-            composeMany [
-              enableSharedExecutables
-              enableSharedLibraries
-              disableStaticLibraries
-              disableLibraryProfiling
-            ];
+          sharedLibExe = composeMany [
+            enableSharedExecutables
+            enableSharedLibraries
+            disableStaticLibraries
+            disableLibraryProfiling
+          ];
         };
-      overlays = {
-        default = _: _: {
-          inherit haskellSharedLibExe;
-        };
-      };
+
+      mkCabal = { packages, ghcVersion ? 924 }:
+        { name, source, exclude ? [ ], dependencies ? { }, configureFlags ? [ ]
+        , extraLibraries ? [ ] }:
+        let
+          haskellPackages =
+            packages.haskell.packages."ghc${toString ghcVersion}";
+        in with packages.lib;
+        with packages.haskell.lib;
+        with nix-filter.lib;
+        addExtraLibraries (appendConfigureFlags (disableLibraryProfiling
+          (disableStaticLibraries (enableSharedLibraries
+            (enableSharedExecutables (haskellPackages.callCabal2nix name
+              (filter {
+                root = source;
+                inherit exclude;
+              }) dependencies))))) configureFlags) extraLibraries;
+
+      overlays = { default = _: _: { inherit haskellSharedLibExe mkCabal; }; };
       lib = rec {
         __functor = self: haskellSharedLibExe;
-        inherit haskellSharedLibExe;
+        inherit haskellSharedLibExe mkCabal;
       };
-    in {
-      inherit overlays lib;
-    };
+    in { inherit overlays lib; };
 }
