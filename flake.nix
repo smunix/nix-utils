3a5f6759d7438a9ac539f956421821d9933b418d
Providence Salumu <smunix@Providences-MacBook-Pro.local>
2023-01-21 07:12:36 -0500

    add fast and slow builds

--- flake.nix
+++ flake.nix
@@ -1,12 +1,47 @@
 {
   description = "nix utils";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=master";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=haskell-updates";
   inputs.nix-filter.url = "github:numtide/nix-filter";
 
   outputs = { self, nixpkgs, nix-filter, ... }@inputs:
     with nixpkgs.lib;
     let
+      # endo :: [drv -> drv] -> drv -> drv
+      endoL = foldr (fn: r: x: fn (r x)) id;
+
+      # applyS :: (drv -> drv') -> {f:drv,...} -> {f:drv'}
+      applyS = fn: attrsets.mapAttrs (_: drv: fn drv);
+
+      # build :: { globalExt } -> hpkgs -> specs -> hpkgs
+      build = { globalExt }:
+        hpkgs: specs:
+        let
+          localExts = composeManyExtensions (map (spec:
+            (hf: hp:
+              applyS (endoL (spec.modifiers or [ ])) (spec.extension hf hp)))
+            specs);
+        in hpkgs.override ({
+          overrides = composeExtensions globalExt localExts;
+        });
+
+      # slow :: hpkgs -> specs -> hpkgs
+      slow = build { globalExt = hf: hp: { }; };
+
+      # fast :: hpkgs -> specs -> hpkgs
+      fast = build {
+        globalExt = hf: hp: {
+          mkDerivation = args:
+            hp.mkDerivation (args // {
+              doCheck = false;
+              doHaddock = false;
+              enableLibraryProfiling = false;
+              enableExecutableProfiling = false;
+              jailbreak = true;
+            });
+        };
+      };
+
       thenEndo = f: g: x: f (g (x));
       manyEndo = foldr thenEndo (x: x);
 
@@ -72,7 +107,7 @@
               version = mkVersion "${old.version}";
             })) configureFlags) extraLibraries);
 
-      lib = { inherit thenEndo manyEndo mkCabal mkCabal0; };
+      lib = { inherit slow fast thenEndo manyEndo mkCabal mkCabal0; };
       overlays = { default = _: _: lib; };
     in { inherit overlays lib; };
 }
